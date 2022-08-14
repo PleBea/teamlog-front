f130f0696c877ad3cbc9621a65cf38577eafc74f
PleBea <bini060315kim@gmail.com>
2022-08-14 10:18:04 +0900

    change css to scss

--- nuxt.config.js
+++ nuxt.config.js
@@ -20,7 +20,7 @@ export default {
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
-    '~/static/stylesheet/global.css'
+    '~/static/stylesheet/global.scss'
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
