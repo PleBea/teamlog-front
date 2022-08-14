62b03bdbbbe7525f506020ab5cbb73c19f10176a
plebea <bini060315kim@gmail.com>
2022-08-14 17:25:59 +0900

    update animation

--- components/Main.vue
+++ components/Main.vue
@@ -3,23 +3,23 @@
     <div class="main_background" :style="config">
       <div class="main_wrapper">
         
-        <div class="main_wrapper_titlebox">
-          <div class="main_wrapper_title">
-            <p v-for="(i, n) in title"
-            :key="n"
-            :class="[i=='LOG' ? 'main_title_sub' : 'main_title']"
-            :style="{animationDelay: `${n*0.2}s`}"
-            >{{ i }}</p>
-          </div>
-          
-          <div class="main_wrapper_subtitle">
-            <p  v-for="(i, k) in subtitle" 
-            :key="k" 
-            :class="[i=='Change' ? 'main_subtitle_sub' : 'main_subtitle']"
-            :style="{animationDelay: `${k*0.2}s`}"
-            >{{ i }}</p>
-          </div>
+
+        <div class="main_wrapper_title">
+          <p v-for="(i, n) in title"
+          :key="n"
+          :class="[i=='LOG' ? 'main_title_sub' : 'main_title']"
+          :style="{animationDelay: `${n*0.2}s`}"
+          >{{ i }}</p>
         </div>
+        
+        <div class="main_wrapper_subtitle">
+          <p  v-for="(i, k) in subtitle" 
+          :key="k" 
+          :class="[i=='Change' ? 'main_subtitle_sub' : 'main_subtitle']"
+          :style="{animationDelay: `${k*0.2}s`}"
+          >{{ i }}</p>
+        </div>
+
 
         <div class="main_wrapper_button">
           <button v-for="(i, g) in button_list"
@@ -43,7 +43,7 @@ export default {
     let title  = ["TEAM", "LOG"]
     let subtitle = ["Feel", "the", "Change", "Make", "The", "Change"]
     let button_list = ["소개", "커리큘럼", "합격자 목록"]
-    let application = [config_json.club_application_start, config_json.club_application_end]
+    let application = [Date.parse(config_json.club_application_start), Date.parse(config_json.club_application_end)]
     let now = new Date()
     let show = false
     config = {
