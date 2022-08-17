e252cf4790dbaa00f1a5f295fbe5728add8a940d
plebea <bini060315kim@gmail.com>
2022-08-18 08:41:35 +0900

    create: successful

--- components/Successful.vue
+++ components/Successful.vue
@@ -1,5 +1,7 @@
 <template>
-
+    <div class="successful">
+        
+    </div>
 </template>
 
 <script>
--- pages/successful.vue
+++ pages/successful.vue
@@ -1,20 +1,16 @@
 <template>
     <div>
-        
+        <Successful></Successful>
     </div>
 </template>
 
 <script>
+import Successful from '../components/Successful.vue';
+
 export default {
-    name: "successful"
+    name: "successful",
+    components: {
+        Successful
+    },
 }
 </script>
-
-<style>
-#successful-box{
-    transform: translate(-50%, -50%);
-    position: absolute;
-    top: 50%;
-    left: 50%;
-}
-</style>
