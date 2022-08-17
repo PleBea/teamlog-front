d17fa48df0e001f5cfee0b6ce699a302ba0c261b
plebea <bini060315kim@gmail.com>
2022-08-18 08:40:14 +0900

    create header

--- components/Header.vue
+++ components/Header.vue
@@ -1,13 +1,31 @@
 <template>
     <div class="header">
-        <p>ㅁㄴㅁ</p>
+        <nuxt-link
+        class="header_left"
+        :to="{path: '/'}" 
+        >
+            <img src="/images/teamlog_logo.png" alt="">
+        </nuxt-link>
+        <div class="header_right">
+            <nuxt-link
+            v-for="(i,k) in header_item"
+            :key="k"
+            :to="{path: i.url}"
+            :class="{header_underline: i.url == $nuxt.$route.path}"
+            >{{ i.name }}</nuxt-link>
+        </div>
     </div>
 </template>
 
 <script>
+import header_item from '@/static/resources/headerItem.json';
+
 export default {
     setup() {
-        
+
+        return {
+            header_item
+        }
     }
 }
 </script>
