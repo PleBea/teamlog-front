0e1a69eb8a67ef23ff2ab6c9785e6fa0bfb208cf
plebea <bini060315kim@gmail.com>
2022-08-23 16:12:35 +0900

    update

--- components/Successful.vue
+++ components/Successful.vue
@@ -1,11 +1,15 @@
 <template>
     <div class="successful">
         <div class="successful_wrapper">
+            <h1 class="successful_title">축하드립니다!</h1>
 
-            <p 
-            v-for="(i, k) in successful"
-            :key="k"
-            >{{ i }}</p>
+            <div class="successful_wrapper_people">
+                <p 
+                class="successful_people"
+                v-for="(i, k) in successful"
+                :key="k"
+                >{{ i }}</p>
+            </div>
 
         </div>
     </div>
